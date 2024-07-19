ee324ef6c9856ac86aa10151f92b029a95c4eff6
Adonay Santos <adonay.rssantos@gmail.com>
2024-07-19 19:36:39 -0300

    Header adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Home from './pages/Home';
+import Header from "./components/Header/Header";
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+	element: 
+	(
+		<>
+			<div>
+				<Header />
+				<Outlet />
+			</div>
+		</>
+	),
+	children:[
+		{
+			path: "/",
+    		element: <Home />,
+		}
+	]
+    
   },
 ]);
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={router} />
-      <ToastContainer />
+		<RouterProvider router={router} />
+      	<ToastContainer />
     </>
   );
 }
